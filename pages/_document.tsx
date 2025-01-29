e72a4e37fa5aa295df1965d5c6a06dd4283b3869
Pruthiviraj <pruthivirajrao91521@gmail.com>
2025-01-30 00:40:37 +0530

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+                  {/* Google Font Link */}
+                  <link
+            href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ pages/offers/index.tsx
@@ -0,0 +1,148 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const Offers = () => {
+  return (
+    <Box sx={{ border: "1px solid black", width: "100%" }}>
+      <Box
+        sx={{
+          color: "#57585B",
+          border: "1px solid black",
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Typography
+            component="span"
+            sx={{
+              fontFamily: "Jost",
+              fontSize: "1.77vw",
+              fontWeight: 700,
+              lineHeight: "1.77vw",
+              letterSpacing: "0.2em",
+              textAlign: "left",
+            }}
+          >
+            HANDPICKED OFFERS{" "}
+          </Typography>
+          <Typography
+            component="span"
+            sx={{
+              fontFamily: "Jost",
+              fontSize: "1.77vw",
+              fontWeight: 400,
+              lineHeight: "1.77vw",
+              letterSpacing: "0.2em",
+            }}
+          >
+            FOR YOUR DREAM DESTINATION
+          </Typography>
+        </Box>
+
+        {/* Centered Typography */}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            overflow: "hidden",
+          }}
+        >
+          <Typography sx={{ width: "43.4375vw", textAlign: "center" }}>
+            Discover special deals at SeleQtions hotels that take you closer to
+            the destinations you've always dreamed of—without compromising on
+            comfort or style.
+          </Typography>
+        </Box>
+        <Box sx={{ width: "97vw", border: "1px solid black",display:"flex" }}>
+          <Box>
+            <Box sx={{ width: "31.927vw", textAlign: "center" }}>
+              {/* Background image setup */}
+              <Box
+                sx={{
+                  width: "29.844vw",
+                  height: "17.76vw", // Define a height for the background
+                  backgroundImage: `url("https://s3-alpha-sig.figma.com/img/36a2/3aa4/090806b51e52a999223c34e2bee7ea0d?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UXBxn888TMaPSgjdZSuOKCWzH6Du207czcnV5jDqj7vuKBYnrjtL-dvZeWTeAzmD9xRsemwkCKd1brCjCtu6Lkh~W1cvof1TtV1zWkFhdy1sDYqdq5I~VvzV~XXxaCIJ9fXfT7gHK03VC-Uu~7rSD7ljiMDW-LGUnpgIpIND03DUBhCDN5ze3xrjfKQWah2i10ZTa59aiCUJufnBPKUwFN1zC0HZllgBdos4t80-yQkNQUGJOHDJbK17Wfs4PWceTNd8ZYZb7fiwrRasnteKE9YnqCK1DqHyq2gpk8l2TuWt625AXkecJIbPWndUYx7q8yCMvXwMUwjLCKi6~EKRJQ__")`,
+                  backgroundSize: "cover", // Ensure the image covers the box
+                  backgroundPosition: "center", // Center the image
+                  backgroundRepeat: "no-repeat", // Avoid repetition of the image
+                }}
+              />
+              <Box>
+                
+                <Typography></Typography>
+                <Typography></Typography>
+              </Box>
+            </Box>
+          </Box>
+          <Box>
+            <Box sx={{ width: "31.927vw", textAlign: "center" }}>
+              {/* Background image setup */}
+              <Box
+                sx={{
+                  width: "29.844vw",
+                  height: "17.76vw", // Define a height for the background
+                  backgroundImage: `url("https://s3-alpha-sig.figma.com/img/36a2/3aa4/090806b51e52a999223c34e2bee7ea0d?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UXBxn888TMaPSgjdZSuOKCWzH6Du207czcnV5jDqj7vuKBYnrjtL-dvZeWTeAzmD9xRsemwkCKd1brCjCtu6Lkh~W1cvof1TtV1zWkFhdy1sDYqdq5I~VvzV~XXxaCIJ9fXfT7gHK03VC-Uu~7rSD7ljiMDW-LGUnpgIpIND03DUBhCDN5ze3xrjfKQWah2i10ZTa59aiCUJufnBPKUwFN1zC0HZllgBdos4t80-yQkNQUGJOHDJbK17Wfs4PWceTNd8ZYZb7fiwrRasnteKE9YnqCK1DqHyq2gpk8l2TuWt625AXkecJIbPWndUYx7q8yCMvXwMUwjLCKi6~EKRJQ__")`,
+                  backgroundSize: "cover", // Ensure the image covers the box
+                  backgroundPosition: "center", // Center the image
+                  backgroundRepeat: "no-repeat", // Avoid repetition of the image
+                }}
+              />
+              <Box>
+                
+                <Typography></Typography>
+                <Typography></Typography>
+              </Box>
+            </Box>
+          </Box>
+          <Box>
+            <Box sx={{ width: "31.927vw", textAlign: "center" }}>
+              {/* Background image setup */}
+              <Box
+                sx={{
+                  width: "29.844vw",
+                  height: "17.76vw", // Define a height for the background
+                  backgroundImage: `url("https://s3-alpha-sig.figma.com/img/36a2/3aa4/090806b51e52a999223c34e2bee7ea0d?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UXBxn888TMaPSgjdZSuOKCWzH6Du207czcnV5jDqj7vuKBYnrjtL-dvZeWTeAzmD9xRsemwkCKd1brCjCtu6Lkh~W1cvof1TtV1zWkFhdy1sDYqdq5I~VvzV~XXxaCIJ9fXfT7gHK03VC-Uu~7rSD7ljiMDW-LGUnpgIpIND03DUBhCDN5ze3xrjfKQWah2i10ZTa59aiCUJufnBPKUwFN1zC0HZllgBdos4t80-yQkNQUGJOHDJbK17Wfs4PWceTNd8ZYZb7fiwrRasnteKE9YnqCK1DqHyq2gpk8l2TuWt625AXkecJIbPWndUYx7q8yCMvXwMUwjLCKi6~EKRJQ__")`,
+                  backgroundSize: "cover", // Ensure the image covers the box
+                  backgroundPosition: "center", // Center the image
+                  backgroundRepeat: "no-repeat", // Avoid repetition of the image
+                }}
+              />
+              <Box>
+                
+                <Typography></Typography>
+                <Typography></Typography>
+              </Box>
+            </Box>
+          </Box>
+          <Box>
+            <Box sx={{ width: "31.927vw", textAlign: "center" }}>
+              {/* Background image setup */}
+              <Box
+                sx={{
+                  width: "29.844vw",
+                  height: "17.76vw", // Define a height for the background
+                  backgroundImage: `url("https://s3-alpha-sig.figma.com/img/36a2/3aa4/090806b51e52a999223c34e2bee7ea0d?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UXBxn888TMaPSgjdZSuOKCWzH6Du207czcnV5jDqj7vuKBYnrjtL-dvZeWTeAzmD9xRsemwkCKd1brCjCtu6Lkh~W1cvof1TtV1zWkFhdy1sDYqdq5I~VvzV~XXxaCIJ9fXfT7gHK03VC-Uu~7rSD7ljiMDW-LGUnpgIpIND03DUBhCDN5ze3xrjfKQWah2i10ZTa59aiCUJufnBPKUwFN1zC0HZllgBdos4t80-yQkNQUGJOHDJbK17Wfs4PWceTNd8ZYZb7fiwrRasnteKE9YnqCK1DqHyq2gpk8l2TuWt625AXkecJIbPWndUYx7q8yCMvXwMUwjLCKi6~EKRJQ__")`,
+                  backgroundSize: "cover", // Ensure the image covers the box
+                  backgroundPosition: "center", // Center the image
+                  backgroundRepeat: "no-repeat", // Avoid repetition of the image
+                }}
+              />
+              <Box>
+                
+                <Typography></Typography>
+                <Typography></Typography>
+              </Box>
+            </Box>
+          </Box>
+
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Offers;
